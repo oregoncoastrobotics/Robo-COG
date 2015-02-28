2f89925c22a6bd2ab2b8a820b43ff359e1be8bce
Wil Black <wilblack21@gmail.com>
2015-02-28 10:19:42 -0800

    Update installer.sh

--- 30-Software_Design/10-Vision/send_stream/installer.sh
+++ 30-Software_Design/10-Vision/send_stream/installer.sh
@@ -8,8 +8,8 @@ sudo apt-get install libv4l-dev -y
 mkdir ~/bin
 mkdir /tmp
 
-cp send_stream/build_send_stream.sh /tmp
-cp send_stream/send_stream.c /tmp
+cp build_send_stream.sh /tmp
+cp send_stream.c /tmp
 
 # You may need to adjust 
 /tmp/send_stream -c0 -s -f -d /dev/video0
